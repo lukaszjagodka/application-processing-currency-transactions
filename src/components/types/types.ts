324682d9a3c3c60837743efac862cf67914bfdbf
encorey <kosz.kosz@protonmail.com>
2022-05-18 00:47:33 +0200

    Add types

---
+++ src/components/types/types.ts
@@ -0,0 +1,15 @@
+export type TNewTransaction = {
+  id: string,
+  amount: number,
+  description: string,
+  createdAt: Date
+};
+
+export type TTransactionAction = {
+  type: string,
+  payload?: any
+};
+
+export type TTransactions = {
+  transactions: Array<TNewTransaction>
+};
