bc20a266d017d8cc9f0ea3d58b8fa30fa2576616
encorey <kosz.kosz@protonmail.com>
2022-05-17 18:48:28 +0200

    Connect Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './AppReducer';
+
+const store = createStore(
+  rootReducer,
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
