f864e3c9fc854c70915687f9a74ebbabb4d6d17d
encorey <kosz.kosz@protonmail.com>
2022-05-17 18:43:23 +0200

    Add TransactionItem component

--- src/components/transactions/ Transaction /TransactionItem.tsx
+++ src/components/transactions/ Transaction /TransactionItem.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
-export default function TransactionItem() {
+import './TransactionItem.css';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
+function TransactionItem() {
+  const [euro, setEuro] = useState<number>(0);
+  const [pln, setPln] = useState<number>(0);
   return (
-    <div>TransactionItem</div>
+    <div className="transaction-item-container">
+      <div className="transaction-item-name">
+        <ThemeProvider theme={theme}>
+          <Typography className="transaction-item-name-typo" variant="h5">Transaction name</Typography>
+        </ThemeProvider>
+      </div>
+      <div className="amount">
+        <p className="amount-typo-euro">
+          {euro}
+          {' '}
+          Euro -
+        </p>
+      </div>
+      <div className="amount">
+        <p className="amount-typo-pln">
+          {pln}
+          {' '}
+          Pln
+        </p>
+      </div>
+    </div>
   );
 }
+
+export default TransactionItem;
