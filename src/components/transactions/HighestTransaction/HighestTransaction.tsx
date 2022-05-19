36360937e59a536862ea16f29a7e4e0dbbeffd39
encorey <kosz.kosz@protonmail.com>
2022-05-19 11:00:44 +0200

    Add amount in pln to highestTransaction

--- src/components/transactions/HighestTransaction/HighestTransaction.tsx
+++ src/components/transactions/HighestTransaction/HighestTransaction.tsx
@@ -15,6 +15,7 @@ theme = responsiveFontSizes(theme);
 
 function HighestTransaction() {
   const transactions = useSelector((state: TTransactionsOnList) => state.transactions);
+  const account = useSelector((state: any) => state.account);
   return (
     <div className="highest-transaction-container">
       <div className="h-t-name">
@@ -22,26 +23,43 @@ function HighestTransaction() {
           <Typography variant="h4">Highest transaction</Typography>
         </ThemeProvider>
       </div>
-      <div className="h-t-amount">
-        <ThemeProvider theme={theme}>
-          <Typography variant="h5">
-            Amount:
-            {' '}
-            <b>{transactions.highest.amount}</b>
-            {' '}
-            <b>Euro</b>
-          </Typography>
-        </ThemeProvider>
-      </div>
-      <div className="h-t-description">
-        <ThemeProvider theme={theme}>
-          <Typography variant="h5">
-            Description:
-            {' '}
-            <b>{transactions.highest.description}</b>
-          </Typography>
-        </ThemeProvider>
-      </div>
+      {
+        transactions.transactions.length !== 0 ? (
+          <>
+            <div className="h-t-amount">
+              <ThemeProvider theme={theme}>
+                <Typography variant="h5">
+                  Amount:
+                  {' '}
+                  <b>{transactions.highest.amount}</b>
+                  {' '}
+                  <b>EUR</b>
+                </Typography>
+              </ThemeProvider>
+            </div>
+            <div className="h-t-amount">
+              <ThemeProvider theme={theme}>
+                <Typography variant="h5">
+                  Amount:
+                  {' '}
+                  <b>{(transactions.highest.amount * account.course).toFixed(2)}</b>
+                  {' '}
+                  <b>PLN</b>
+                </Typography>
+              </ThemeProvider>
+            </div>
+            <div className="h-t-description">
+              <ThemeProvider theme={theme}>
+                <Typography variant="h5">
+                  Description:
+                  {' '}
+                  <b>{transactions.highest.description}</b>
+                </Typography>
+              </ThemeProvider>
+            </div>
+          </>
+        ) : ''
+      }
     </div>
   );
 }
