814de7b3a9e7b2cfc1f25759b27a0403fd1add5e
encorey <kosz.kosz@protonmail.com>
2022-05-19 09:57:04 +0200

    Connect accountReducer and redux-logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import { updateCourse } from './components/accountActions';
 import Dashboard from './components/Dashboard/Dashboard';
+import { fetchCourse } from './helpers/fetchCourse';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const objCourse = fetchCourse();
+    objCourse.then((result) => {
+      dispatch(updateCourse(result));
+    });
+  }, []);
+
   return (
     <div className="App">
       <Dashboard />
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './AppReducer';
 
 const store = createStore(
   rootReducer,
+  composeWithDevTools(
+    applyMiddleware(logger),
+  ),
 );
 
 const root = ReactDOM.createRoot(
