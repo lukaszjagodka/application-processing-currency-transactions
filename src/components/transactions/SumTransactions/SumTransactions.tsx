8896f1380c3daaa5f637827277ba047f67990548
encorey <kosz.kosz@protonmail.com>
2022-05-19 09:53:00 +0200

    Add SumTransactions component

---
+++ src/components/transactions/SumTransactions/SumTransactions.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { TTransactionsOnList } from '../../types/types';
+
+import './SumTransaction.css';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
+function SumTransaction() {
+  const transactions = useSelector((state: TTransactionsOnList) => state.transactions);
+  return (
+    <div className="sum-transaction-container">
+      <div className="sum-transaction-name">
+        <ThemeProvider theme={theme}>
+          <Typography variant="h5">
+            Sum of all transactions -
+            {' '}
+            <b>{(transactions.sum).toFixed(2)}</b>
+            {' '}
+            <b>EUR</b>
+            {' '}
+            with
+            {' '}
+            <b>{transactions.transactions.length}</b>
+            {' '}
+            transactions
+          </Typography>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+}
+
+export default SumTransaction;
